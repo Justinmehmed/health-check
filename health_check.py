77d8a9e6fb71e9f412511559d32cab8cd119d37d
Justinmehmed <justin.mehmed@yahoo.com>
2024-04-21 18:22:22 +1000

    pytz-added

--- health_check.py
+++ health_check.py
@@ -1,13 +1,15 @@
 import requests
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
 
 def check_service(url):
     try:
         response = requests.get(url + "/now")
         if response.status_code == 200:
-            current_time_str = response.text.strip()
-            current_time = datetime.strptime(current_time_str, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
-            now = datetime.now(timezone.utc)
+            current_time = datetime.strptime(response.text, "%Y-%m-%d %H:%M:%S")
+            current_time = pytz.utc.localize(current_time)
+            current_time = current_time.astimezone(pytz.utc)
+            now = datetime.now(pytz.utc)
             time_difference = abs(now - current_time).total_seconds()
             if time_difference <= 1:
                 return True
